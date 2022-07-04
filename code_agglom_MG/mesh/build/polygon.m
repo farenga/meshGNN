5017e254180ec141a378fc9eb2a3d8016cf6c631
farenga <nicola.farenga@gmail.com>
2022-07-04 13:11:41 +0000

    update

--- code_agglom_MG/mesh/build/polygon.m
+++ code_agglom_MG/mesh/build/polygon.m
@@ -1,5 +1,5 @@
 function [mesh] = polygon(N)
-pgon = nsidedpoly(N); % center in (0,0) and radius 1
+pgon = nsidedpoly(N);  % center in (0,0) and radius 1
 mesh = RMesh(2);
 for i = 1:N
     add_new0(mesh,pgon.Vertices(i,:));
